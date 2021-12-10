c6015edd35dc09126a3cc1ea6e622a1a9cfee66d
PiKouri <hok@etud.insa-toulouse.fr>
2021-12-10 11:00:13 +0100

    chore: LightSensorService

---
+++ LightSensorService/src/main/java/fr/insa/arm/LightSensorService/LightSensorServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.arm.LightSensorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightSensorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightSensorServiceApplication.class, args);
+	}
+
+}
